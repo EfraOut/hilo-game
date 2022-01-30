a25ea97907ea895258ef05408ba07ce293a493b2
Efrain Arturo Gomez Fajardo <gom21023@byui.edu>
2022-01-30 10:25:45 -0700

    Missing to change the logic in the Compare()

--- Game/Cards.cs
+++ Game/Cards.cs
@@ -1,4 +1,36 @@
-public class Cards
+// A card is a plastic piece of paper that contains a numeric value
+//
+// The responsabilities of a card is to draw a random card and keep track whether is higher or lower than other card
+public class Card
 {
-    
+    public int nextCard = 0;
+    public int currentCard = 0;
+
+    // Constructs a new instance of Cards
+    public Card()
+    {
+    }
+
+    // Gives the value of the first card
+    public int Deal()
+    {
+        Random random = new Random();
+        currentCard = random.Next(1, 13);
+        return currentCard;
+    }
+
+    // Deals the second card and compares it with the first one
+    public string Compare()
+    {
+        nextCard = this.Deal();
+        
+        if (nextCard < currentCard)
+        {
+            return "l";
+        }
+        else
+        {
+            return "h";
+        }
+    }
 }
--- Game/Director.cs
+++ Game/Director.cs
@@ -1,4 +1,53 @@
 public class Director
 {
-    
+    string userChoice = "";
+    Card card = new Card();
+    bool isPlaying = true;
+    int score = 300;
+    string keepPlaying = "";
+
+    public void StartGame()
+    {
+        while (isPlaying && score != 0)
+        {
+            GetInputs();
+            CompareCards();
+            DoOutputs();
+        }
+    }
+
+    // Asks the user their guess.
+    public void GetInputs()
+    {   
+        int firstCard = card.Deal();
+        Console.WriteLine($"The card is: {firstCard}");
+        Console.Write("Higher or Lower? [h/l] ");
+        string userChoice = Console.ReadLine();
+    }
+
+    // Compares the tw cards, depending on what the users inputs
+    public void CompareCards()
+    {
+        string compare = card.Compare();
+        Console.WriteLine(compare);
+
+        if (compare == userChoice)
+        {
+            score += 100;
+        }
+        else
+        {
+            score -= 75;
+        }
+    }
+
+    // Prints on the console the results and asks if they want to keep playing.
+    public void DoOutputs()
+    {
+        Console.WriteLine($"The next card was: {card.nextCard}");
+        Console.WriteLine($"Your score is: {score}");
+        Console.Write($"Keep Playing? [y/n]: ");
+        keepPlaying = Console.ReadLine();
+        isPlaying = (keepPlaying == "y");
+    }
 }
